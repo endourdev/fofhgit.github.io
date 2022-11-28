4a26e9e061fecdf099103d72c871ed896147a7b9
fofhgit <ouaoudamohamed448@gmail.com>
2022-11-28 17:29:29 +0100

    Fix More Bugs

--- api/adblock/tkt.js
+++ api/adblock/tkt.js
@@ -3,3 +3,19 @@ const adblock_off = "Veuillez désactivez AdBlock pour le bon fonctionnement du 
 
 console.log(adblock), alert(adblock)
 console.warn(adblock_off);
+
+const api = "Appel d'api fait."
+
+console.log(api);
+
+const mtn = "L'api est en maintenance."
+const mtnt = true
+const mon = "L'api n'est pas en maintenance."
+
+const api2 = "Appel d'api fait."
+
+if (mtnt === true) {
+    console.warn(mtn)
+} else {
+    console.log(mon)
+}
